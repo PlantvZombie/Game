65b3587ba9cc7f50f9577952a4b703d7f2f14407
Crabion78 <149846477+Crabion78@users.noreply.github.com>
2024-09-04 23:51:54 -0400

    distance calc

--- character.gd
+++ character.gd
@@ -18,13 +18,22 @@ func _physics_process(delta):
 	if  is_on_floor() and (Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")):
 		velocity.y = JUMP_VELOCITY
 	if grappleTargets.size() > 0 and not is_on_floor() and (Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")):
-			print(grappleTargets[0].position)
-			print(grappleTargets)
+		
+		var grappleDistance = -1
+		for j in grappleTargets.size():
+			if sqrt((grappleTargets[j].x)^2) > grappleDistance:
+				pass
+		print(grappleTargets[0].position)
 
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * SPEED, 80)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 65)
-
+	
 	move_and_slide()
+	
+func distance(x0, y0, x1, y1):
+	return sqrt((x0 - x1)**2+(y0-y1)**2)
+
+
