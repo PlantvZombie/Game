39b8ccdb8b9e18dbfec03405bf3758b6639a6e08
Crabion78 <149846477+Crabion78@users.noreply.github.com>
2024-09-05 14:50:10 -0400

    some stuff

--- character.gd
+++ character.gd
@@ -5,6 +5,7 @@ const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 var grapplingHook = true
 var grappleTargets = []
+var grappleTarget
 
 var left:bool = false
 var right:bool = false
@@ -16,6 +17,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("CollisionShape2D/Sprite2D")
 
 func _physics_process(delta):
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -23,13 +26,21 @@ func _physics_process(delta):
 	# Handle Jump.
 	if  is_on_floor() and (Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")):
 		velocity.y = JUMP_VELOCITY
-	if grappleTargets.size() > 0 and not is_on_floor() and (Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")):
 		
-		var grappleDistance = -1
-		for j in grappleTargets.size():
-			if sqrt((grappleTargets[j].x)^2) > grappleDistance:
-				pass
-		print(grappleTargets[0].position)
+#	if grappleTargets.size() > 0 and not is_on_floor() and (Input.is_action_just_pressed("up") or Input.is_action_just_pressed("space")):
+#
+#		if not grappleTarget == null:
+#			grappleTarget.spriteOn(false)
+#
+#		var closestTarget = grappleTargets[0]
+#		closestTarget.spriteOn(true)
+#		for j in grappleTargets.size() -1:
+#				if distance(grappleTargets[j+1].position.x,grappleTargets[j+1].position.y,self.position.x, self.position.y) < distance(closestTarget.position.x,closestTarget.position.y,self.position.x, self.position.y) :
+#					closestTarget.spriteOn(false)
+#					closestTarget = grappleTargets[j+1]
+#					closestTarget.spriteOn(true)
+#		grappleTarget = closestTarget
+		
 
 	var direction = Input.get_axis("left", "right")
 	if direction:
@@ -59,4 +70,18 @@ func _physics_process(delta):
 func distance(x0, y0, x1, y1):
 	return sqrt((x0 - x1)**2+(y0-y1)**2)
 
+func _process(delta):
+	grappleTargets[0].spriteOn(false)
+	if grappleTargets.size() > 3:
+		if not grappleTarget == null:
+			grappleTarget.spriteOn(false)
+			
+		var closestTarget = grappleTargets[0]
+		closestTarget.spriteOn(true)
+		for j in grappleTargets.size() -1:
+				if distance(grappleTargets[j+1].position.x,grappleTargets[j+1].position.y,self.position.x, self.position.y) < distance(closestTarget.position.x,closestTarget.position.y,self.position.x, self.position.y) :
+					closestTarget.spriteOn(false)
+					closestTarget = grappleTargets[j+1]
+					closestTarget.spriteOn(true)
+		grappleTarget = closestTarget
 
