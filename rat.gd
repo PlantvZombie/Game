dc4be659dafbbaf096ac2a7d6a105e164064794b
PlantvZombie <wintonajones@gmail.com>
2024-09-06 18:49:55 -0400

    Rat

--- rat.gd
+++ rat.gd
@@ -1,9 +1,41 @@
-extends Area2D
+extends AnimatableBody2D
+
+@onready var anim = get_node("CollisionShape2D/Rat")
+var ChaseMode:bool = false
+var AnimOver:bool = true
+signal StartMovement
 
 
+func _physics_process(_delta):
+	if ChaseMode:
+		position.x = move_toward(position.x, get_node("/root/Global").PlayerPos.x, 1)
+		if (position.x - get_node("/root/Global").PlayerPos.x) < 0:
+			anim.play("WalkRight")
+		elif (position.x - get_node("/root/Global").PlayerPos.x) > 0:
+			anim.play("WalkLeft")
+	if AnimOver and !ChaseMode:
+		anim.play("WalkRight")
+		AnimOver = false
+		StartMovement.emit()
 
 
-
-func _on_body_entered(body):
+func _on_area_2d_body_entered(body):
 	if body.name == "Character":
-		get_tree().change_scene_to_file("res://test.tscn")
+		ChaseMode = true
+
+
+func _on_start_movement():
+	var tween = create_tween()
+	tween.tween_property(self, "position", Vector2(position.x + 100, position.y), 1)
+	await get_tree().create_timer(1).timeout
+	tween.stop()
+	anim.play("WalkLeft")
+	get_node("Area2D").rotate(PI)
+	get_node("Area2D/POV").position.x += 1
+	tween = create_tween()
+	tween.tween_property(self, "position", Vector2(position.x - 100, position.y), 1)
+	await get_tree().create_timer(1).timeout
+	tween.stop()
+	AnimOver = true
+	get_node("Area2D").rotate(PI)
+	get_node("Area2D/POV").position.x -= 1
