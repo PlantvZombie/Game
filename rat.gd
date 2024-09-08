5f0b9ed94a9e190c97acda81308be18aefd9463a
PlantvZombie <wintonajones@gmail.com>
2024-09-07 22:31:47 -0400

    Hit Satisfaction & Death Anim

--- rat.gd
+++ rat.gd
@@ -7,14 +7,17 @@ signal StartMovement
 var right:bool = false
 var left:bool = false
 var tween
+var TweenIsBlank:bool = true
 var FirstRun:bool = true
-var health:int = 10
+var health:int = 20
 var MoveDist:int = 100
-
+var Dead:bool = false
+signal Death
 
 func _physics_process(_delta):
-	if ChaseMode:
-		tween.stop()
+	if ChaseMode and !Dead:
+		if !TweenIsBlank:
+			tween.stop()
 		position.x = move_toward(position.x, get_node("/root/Global").PlayerPos.x, 3)
 		if (position.x - get_node("/root/Global").PlayerPos.x) < 0:
 			anim.play("WalkRight")
@@ -24,14 +27,21 @@ func _physics_process(_delta):
 			anim.play("WalkLeft")
 			get_node("Sight").set_rotation(PI)
 			get_node("CloseDetection").set_rotation(PI)
-	if AnimOver and !ChaseMode:
+	if health < 1 and !Dead:
+		if (position.x - get_node("/root/Global").PlayerPos.x) < 0:
+			anim.play("DeathRight")
+			Death.emit()
+			Dead = true
+		elif (position.x - get_node("/root/Global").PlayerPos.x) > 0:
+			anim.play("DeathLeft")
+			Death.emit()
+			Dead = true
+	if AnimOver and !ChaseMode and !Dead:
 		anim.play("WalkRight")
 		right = true
 		left = false
 		AnimOver = false
 		StartMovement.emit()
-	if health < 1:
-		self.queue_free()
 	move_and_slide()
 
 
@@ -44,20 +54,35 @@ func _on_start_movement():
 	if FirstRun:
 		await get_tree().create_timer(1).timeout
 		FirstRun = false
+	if Dead: return
+	TweenIsBlank = false
 	tween = create_tween()
-	tween.tween_property(self, "position", Vector2(position.x + MoveDist, position.y), (MoveDist/100))
-	await get_tree().create_timer(1).timeout
+	tween.tween_property(self, "position", Vector2(position.x + MoveDist, position.y), 1.2)
+	await get_tree().create_timer(1.2).timeout
 	tween.stop()
+	if Dead or ChaseMode: return
 	anim.play("WalkLeft")
 	left = true
 	right = false
 	get_node("Sight").rotate(PI)
 	get_node("CloseDetection").rotate(PI)
+	if Dead: return
 	tween = create_tween()
-	tween.tween_property(self, "position", Vector2(position.x - MoveDist, position.y), (MoveDist/100))
-	await get_tree().create_timer(1).timeout
+	tween.tween_property(self, "position", Vector2(position.x - MoveDist, position.y), 1.2)
+	await get_tree().create_timer(1.2).timeout
 	tween.stop()
 	AnimOver = true
 	get_node("Sight").rotate(PI)
 	get_node("CloseDetection").rotate(PI)
 
+func _on_death():
+	await get_tree().create_timer(1.2).timeout
+	self.queue_free()
+
+
+func _on_character_attacked():
+	get_node("AnimationPlayer").play("flash")
+	if !Dead:
+		Engine.time_scale = 0.07
+		await get_tree().create_timer(0.07 * 0.3).timeout
+		Engine.time_scale = 1
